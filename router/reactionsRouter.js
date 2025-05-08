dbd9ef4f554da4f4f6fbd78932a5f4f674ff7f90
SebastianMayorgaa <jsebastianrmy@gmail.com>
2025-05-08 17:38:27 -0600

    CRUD reaction

--- router/reactionsRouter.js
+++ router/reactionsRouter.js
@@ -1,8 +1,14 @@
 import { Router } from "express";
 import allReactions from "../controllers/reaction/read.js";
+import create from "../controllers/reaction/create.js";
+import update from "../controllers/reaction/update.js";
+import deleteReaction from "../controllers/reaction/delete.js";
 
 let reactionsRouter = Router()
 
 reactionsRouter.get('/allReactions', allReactions)
+reactionsRouter.post('/create', create)
+reactionsRouter.post('/update/:id', update)
+reactionsRouter.delete('/delete/:id', deleteReaction)
 
 export default reactionsRouter
