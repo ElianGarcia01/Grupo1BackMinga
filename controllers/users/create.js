48430aafa7a6ee086dadd1ad0b8ae11a276c5f3b
Yeison Lopez <andislopez13@gmail.com>
2025-05-14 23:15:01 -0500

    Se ajusta la respuesta del registro de usuario para que solo devuelta el token, id, email, photo y role

--- controllers/users/create.js
+++ controllers/users/create.js
@@ -3,11 +3,16 @@ import User from '../../models/User.js'
 let register = async (req, res, next) => {
     try {
         req.user.online = true
-        let user = await User.create(req.user)
+        let userResponse = await User.create(req.user)
         return res.status(201).json({
             response: {
                 token: req.token,
-                user
+                user: {
+                    _id: userResponse._id,
+                    email: userResponse.email,
+                    photo: userResponse.photo,
+                    role: userResponse.role
+                }
             }
         })
     } catch (error) {
