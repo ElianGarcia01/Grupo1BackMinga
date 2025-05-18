e593df38b85bb3932282070c7b0dcf9eb2e8c9e3
Yeison Lopez <andislopez13@gmail.com>
2025-05-18 10:31:16 -0500

    se trabaja en las reacciones y favoritos

--- controllers/favorite/delete.js
+++ controllers/favorite/delete.js
@@ -1,11 +1,19 @@
 import Favorite from "../../models/Favorite.js"
+import Reaction from "../../models/Reaction.js"
 
 let deleteFavorite = async (req, res, next) => {
     try {
-        req.body.user_id = req.user._id
-        let deleteFavorite = await Favorite.deleteOne(req.body)
+        const favoriteData = {};
+        favoriteData.manga_id = req.body.manga_id
+        if(req.roleInfo.author) favoriteData.author_id = req.roleInfo.author._id
+        if(req.roleInfo.company) favoriteData.company_id = req.roleInfo.company._id
+
+        let deleteFavorite = await Favorite.deleteOne(favoriteData)
+        const deletedReaction = await Reaction.deleteOne(favoriteData)
         return res.status(200).json({
-            response: deleteFavorite
+            message: "Favorite and reaction deleted",
+            deleteFavorite,
+            deletedReaction
         })
     } catch (error) {
         next(error)
