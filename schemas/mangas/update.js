7d33f62f3bdfdd4c34b72431d4853c9ebe22d741
SebastianMayorgaa <jsebastianrmy@gmail.com>
2025-05-19 08:57:14 -0600

    Schema

--- schemas/mangas/update.js
+++ schemas/mangas/update.js
@@ -1,27 +1,35 @@
 import joi from "joi-oid";
 
 const schema = joi.object({
+    _id: joi.objectId().required().messages({
+        'string.pattern.name': '_id ID must be a valid ObjectId',
+    }),
+
+    author_id: joi.objectId().messages({
+        'string.pattern.name': 'author_id ID must be a valid ObjectId',
+    }),
+
     title: joi.string().min(3).max(100).messages({
-            'string.base': 'Title must be a string',
-            'string.min': 'Title must have at least 3 characters',
-            'string.max': 'Title must have at most 100 characters',
-        }),
+        'string.base': 'Title must be a string',
+        'string.min': 'Title must have at least 3 characters',
+        'string.max': 'Title must have at most 100 characters',
+    }),
 
     cover_photo: joi.string().uri().pattern(/\.(jpeg|jpg|png|webp)$/i).messages({
-            'string.base': 'Cover photo must be a string (URL)',
-            'string.uri': 'Cover photo must be a valid URL',
-            'string.pattern.base': 'Cover photo must be a valid image link (.jpg, .jpeg, .png, .webp)',
-        }),
+        'string.base': 'Cover photo must be a string (URL)',
+        'string.uri': 'Cover photo must be a valid URL',
+        'string.pattern.base': 'Cover photo must be a valid image link (.jpg, .jpeg, .png, .webp)',
+    }),
 
     description: joi.string().min(10).max(1000).messages({
-            'string.base': 'Description must be a string',
-            'string.min': 'Description must have at least 10 characters',
-            'string.max': 'Description must have at most 1000 characters',
-        }),
+        'string.base': 'Description must be a string',
+        'string.min': 'Description must have at least 10 characters',
+        'string.max': 'Description must have at most 1000 characters',
+    }),
 
     category_id: joi.objectId().required().messages({
-            'string.pattern.name': 'Category ID must be a valid ObjectId',
-        }),
+        'string.pattern.name': 'Category ID must be a valid ObjectId',
+    }),
 });
 
 export default schema;
