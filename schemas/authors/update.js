931fc9e1a1738d284aec513e4d1ddeff74646557
Elian Garcia <telljua90@gmail.com>
2025-05-20 00:35:51 -0600

    schema de validacion de updateAuthor

--- schemas/authors/update.js
+++ schemas/authors/update.js
@@ -6,10 +6,24 @@ const schema = joi.object({
     }),
     
     photo: joi.string().uri().pattern(/\.(jpeg|jpg|png|webp)$/i).required().messages({
-            'string.base': 'Photo must be a string (URL)',
-            'string.uri': 'Photo must be a valid URL',
-            'string.pattern.base': 'The photo link must end with .jpg, .jpeg, .png, or .webp',
-        })
+        'string.base': 'Photo must be a string (URL)',
+        'string.uri': 'Photo must be a valid URL',
+        'string.pattern.base': 'The photo link must end with .jpg, .jpeg, .png, or .webp',
+    }),
+
+    // Nuevos campos permitidos ↓
+    name: joi.string().allow("").optional().messages({
+        'string.base': 'Name must be a string',
+    }),
+    lastName: joi.string().allow("").optional().messages({
+        'string.base': 'Last name must be a string',
+    }),
+    city: joi.string().allow("").optional().messages({
+        'string.base': 'City must be a string',
+    }),
+    date_birth: joi.date().allow(null).optional().messages({
+        'date.base': 'Birth date must be a valid date',
+    }),
 });
 
 export default schema;
