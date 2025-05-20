5f3200fd69d48def513d1e75f245d833bd95370e
Elian Garcia <telljua90@gmail.com>
2025-05-20 03:56:46 -0600

    cambios en validacions joi

--- schemas/mangas/update.js
+++ schemas/mangas/update.js
@@ -1,27 +1,32 @@
 import joi from "joi-oid";
 
 const schema = joi.object({
+    _id: joi.objectId().required().messages({
+        'string.pattern.name': '_id must be a valid ObjectId',
+        'any.required': '_id is required',
+    }),
+
     title: joi.string().min(3).max(100).messages({
-            'string.base': 'Title must be a string',
-            'string.min': 'Title must have at least 3 characters',
-            'string.max': 'Title must have at most 100 characters',
-        }),
+        'string.base': 'Title must be a string',
+        'string.min': 'Title must have at least 3 characters',
+        'string.max': 'Title must have at most 100 characters',
+    }),
 
     cover_photo: joi.string().uri().pattern(/\.(jpeg|jpg|png|webp)$/i).messages({
-            'string.base': 'Cover photo must be a string (URL)',
-            'string.uri': 'Cover photo must be a valid URL',
-            'string.pattern.base': 'Cover photo must be a valid image link (.jpg, .jpeg, .png, .webp)',
-        }),
+        'string.base': 'Cover photo must be a string (URL)',
+        'string.uri': 'Cover photo must be a valid URL',
+        'string.pattern.base': 'Cover photo must be a valid image link (.jpg, .jpeg, .png, .webp)',
+    }),
 
     description: joi.string().min(10).max(1000).messages({
-            'string.base': 'Description must be a string',
-            'string.min': 'Description must have at least 10 characters',
-            'string.max': 'Description must have at most 1000 characters',
-        }),
+        'string.base': 'Description must be a string',
+        'string.min': 'Description must have at least 10 characters',
+        'string.max': 'Description must have at most 1000 characters',
+    }),
 
     category_id: joi.objectId().required().messages({
-            'string.pattern.name': 'Category ID must be a valid ObjectId',
-        }),
+        'string.pattern.name': 'Category ID must be a valid ObjectId',
+    }),
 });
 
 export default schema;
